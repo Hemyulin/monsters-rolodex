d449f3f0f12a36cb42fd9653df627d2ce34ea4e9
Daniel Budiansky <daniel@neon.dev>
2023-06-25 08:37:15 +0200

    Unfuck

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,15 +3,23 @@ import { Component } from "react";
 class CardList extends Component {
     render() {
         const { monsters } = this.props;
-        return (<div className="card-list">
-            {monsters.map(monster => (
-                <div className="card-container">
-                    <img alt="monster" src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} />
-                    <h2>{monster.name}</h2>
-                    <p>{monster.email}</p>
-                </div>
-            ))}
-        </div >)
+
+        return (
+            <div className="card-list">
+                {monsters.map((monster) => {
+                    const { name, email, id } = monster;
+
+                    return (
+                        <div className="card-container" key={id}>
+                            <img alt="monster" src={`https://robohash.org/${id}?set=set2&size=180x180`}
+                            />
+                            <h2>{name}</h2>
+                            <p>{email}</p>
+                        </div>
+                    );
+                })}
+            </div >
+        );
     }
 }
 
